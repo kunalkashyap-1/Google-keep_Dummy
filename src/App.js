ad40ddab6dca06c3e475920d067da00ae0770aa3
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2023-01-30 12:48:32 +0530

    text area

--- src/App.js
+++ src/App.js
@@ -3,19 +3,43 @@ import Header from "./Components/Header"
 import Footer from "./Components/Footer"
 import Note from "./Components/Note"
 import Data from "./Components/NotesData"
-
+import CreateArea from "./Components/CreateArea"
+import React from "react"
 
 
 function App() {
+  const [notes,setNotes] = React.useState([...Data]);
+
+  function addNote(note){
+    // Data.push(note);
+    setNotes(prevNotes =>{
+      if(note.title){
+        return [...prevNotes,note];
+      }else{
+        return[...prevNotes];
+      }
+    });
+  }
+
+  function deleteNote(id){
+    setNotes(prevNotes=>{
+      return prevNotes.filter((noteItem,index)=>{
+        return index!==id;
+      })
+    })
+  }
 
   return (
     <div >
       <Header />
-      {Data.map(_Data => {
+      <CreateArea onAdd={addNote}/>
+      {notes.map((_Data,index) => {
         return <Note
-          key={_Data.key}
+          key={index}
+          id={index}
           heading={_Data.title}
-          body={_Data.content} />
+          body={_Data.content} 
+          onDelete={deleteNote}/>
       })}
       <Footer />
     </div>
--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,8 +1,13 @@
 
 function Note(props){
+   function handleClick(){
+      props.onDelete(props.id);
+   }
+
  return <div className="note">
     <h1>{props.heading}</h1>
     <p>{props.body}</p>
+    <button onClick={handleClick}>DELETE</button>
  </div>
 }
 
